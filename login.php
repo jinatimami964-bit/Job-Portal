dffeff6938b7074544cccf804093b73d5d31d548
Jinat Imami <jinatimami964@gmail.com>
2026-07-26 23:42:33 +0600

    added timeout session

--- login.php
+++ login.php
@@ -21,7 +21,8 @@ if(password_verify($password,$user['password'])){
 
 $_SESSION['user_id']=$user['id'];
 $_SESSION['username']=$user['username'];
-
+// --- CHANGED / ADDED LINE BELOW: Track login timestamp for session timeout ---
+$_SESSION['last_activity'] = time();
 header("Location: dashboard.php");
 exit();
 
