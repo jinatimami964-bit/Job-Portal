2a73aeb18388158d4b6f4ed5721b430969e3f815
Faria-Zaman-Oiky <zamanfaria29@gmail.com>
2026-08-10 22:30:10 +0600

    removed a comment from db_connect.php

--- apply.php
+++ apply.php
@@ -16,7 +16,7 @@ if (!isset($_POST['job_id']) || empty($_POST['job_id'])) {
 $user_id = $_SESSION['user_id'];
 $job_id = $_POST['job_id'];
 
-// PDO Prepared Statement - parameterized query (SQL Injection )
+
 try {
     $stmt = $pdo->prepare("INSERT INTO applications (user_id, job_id) VALUES (:user_id, :job_id)");
     $success = $stmt->execute([
